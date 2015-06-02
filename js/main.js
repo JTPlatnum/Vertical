d1955d1b7949e91b2156a7adc5a25bd95f516a47
JTPlatnum <james@codekings.info>
2015-06-02 14:15:28 -0700

    Fixing header nav dropdown

--- js/main.js
+++ js/main.js
@@ -11,22 +11,46 @@ $( document ).ready(function() {
     });
 
 // start open menu items
-
+    $('.nav li.dropdown ul.dropdown-menu li.figs').on('click', function(){
+        $('main.figs.product-main').toggleClass('active'); 
+    });
 
 // end open menu items
 
 
 
 //start open recipes
+    $('.figs.open-recipe').on('click', function(){
+        $('.figs.product-recipe-hidden').toggleClass('active');
+    });    
+
+    $('.mandarins.open-recipe').on('click', function(){
+        $('.mandarins.product-recipe-hidden').toggleClass('active');
+    });
+    
+    $('.kiwis.open-recipe').on('click', function(){
+        $('.kiwis.product-recipe-hidden').toggleClass('active');
+    });
+    
+    $('.plums.open-recipe').on('click', function(){
+        $('.plums.product-recipe-hidden').toggleClass('active');
+    });
+
     $('.apricots.open-recipe').on('click', function(){
         $('.apricots.product-recipe-hidden').toggleClass('active');
     });
-
-
-    $('.figs.open-recipe').on('click', function(){
-        $('.figs.product-recipe-hidden').toggleClass('active');
+    
+    $('.limes.open-recipe').on('click', function(){
+        $('.limes.product-recipe-hidden').toggleClass('active');
     });
 
+    $('.persimmons.open-recipe').on('click', function(){
+        $('.persimmons.product-recipe-hidden').toggleClass('active');
+    });
+
+    $('.pineapple_quince.open-recipe').on('click', function(){
+        $('.pineapple_quince.product-recipe-hidden').toggleClass('active');
+    });
 //end open recipes
 
 
